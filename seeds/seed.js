5a2823b27f48c79fb41075f7955d832c8743d2f2
ima2132 <ima2132@columbia.edu>
2023-05-29 05:33:30 -0400

    added to seeds.js; completed user data

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Blog} = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const users = await User.findAll();
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
+
+  
